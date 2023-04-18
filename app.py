0e3d284886ea922201f6f8f014bdc3d1c90ebc0f
Dipak Bange <djbange@iu.edu>
2023-04-17 20:41:16 -0400

    added model

--- app.py
+++ app.py
@@ -1,26 +1,47 @@
-from flask import Flask, render_template
-from python_scripts.read_frontend_json import read_json
+from flask import Flask, render_template, request, redirect, url_for
+from python_scripts.utilities import read_json, predict_model, load_data, ohe_data
 
 app = Flask(__name__)
 
+data_loaded = load_data()
 
 @app.route("/", methods=["GET", "POST"])
 def search_page():
-    json_data = read_json()
+    if request.method == "GET":
+        json_data, college_name_list = read_json()
 
-    college_name_list = ["Pune institute of computer technology", "Sinhgad college of Engineering"] 
+        return render_template("search_page.html", facilities_list=sorted(json_data["Facilities_list"]),
+                                genders_accepted_list=sorted(json_data["Genders Accepted"]),
+                                universities_list=sorted(json_data["University"]), 
+                                cities_list=sorted(json_data["City"]),
+                                states_list=sorted(json_data["State"]),
+                                college_type_list=sorted(json_data["College Type"]),
+                                courses_list=sorted(json_data["Courses_list"]),
+                                college_name_list=sorted(college_name_list))
+    elif request.method == "POST":
+        data = {'college_name': 'National Institute of Technology Rourkela '}
+        # data = data.to_dict()
 
-    return render_template("search_page.html", facilities_list=sorted(json_data["Facilities_list"]),
-                            genders_accepted_list=sorted(json_data["Genders Accepted"]),
-                            universities_list=sorted(json_data["University"]), 
-                            cities_list=sorted(json_data["City"]),
-                            states_list=sorted(json_data["State"]),
-                            college_type_list=sorted(json_data["College Type"]),
-                            courses_list=sorted(json_data["Courses_list"]), 
-                            college_name_list=sorted(college_name_list))
+        model_params_dict = data_loaded['model_params_dict'].copy()
+        if data['college_name'] != '':
+            college = data_loaded['train_data'][data_loaded['train_data']['College Name'] == data['college_name']]
+            print(college)
+            college = ohe_data(college)
+            for key, value in college.items():
+                if key in model_params_dict:
+                    model_params_dict[key] = value
+        
+        for key, value in data.items():
+            if key in model_params_dict:
+                model_params_dict[key] = value
+        
+
+        results = predict_model(data_loaded, model_params_dict)
+        return redirect(url_for('results_page', content=results.to_dict()))
 
 
-@app.route('/results')
+
+@app.route('/results', methods=["POST"])
 def results_page():
     content = "results come here in a dictionary"
     return render_template("results_page.html", content=content)
--- python_scripts/utilities.py
+++ python_scripts/utilities.py
@@ -1,28 +1,88 @@
 import json
 import pandas as pd
 import pickle
+from sklearn.neighbors import NearestNeighbors
+import ast
 
 def load_data():
     # constants
-    train_data = pd.read_csv("../data/train.csv")
-    knn_model = pickle.load(open("../data/models/knnpickle_file.pkl", 'rb'))
-    model_params_dict = json.load(open("../data/models/final_param_out.json", 'r'))
+    train_data = pd.read_csv("./data/proccessed_indian_universities.csv").drop(columns=['Unnamed: 0'])
+    train_data['Facilities_list'] =[ast.literal_eval(x) for x in train_data['Facilities_list'] ]
+    train_data['Courses_list'] =[ast.literal_eval(x) for x in train_data['Courses_list'] ]
+    # knn_model = pickle.load(open("./data/models/knnpickle_file.pkl", 'rb'))
+    # model_params_dict = json.load(open("./data/models/final_param_out.json", 'r'))
+
+    engg_colleges = train_data.copy()
+    ohe_prefix_dict = {
+        'Facilities_list': 'facility_',
+        'Courses_list': 'course_',
+        'University': 'uni_',
+        'City': 'city_',
+        'State': 'state_',
+        'College Type': 'ctype_',
+        'Genders Accepted': 'gender_',
+    }
+    for col, pre in ohe_prefix_dict.items():
+        # Apply one-hot encoding on the list in the row
+        college_before = list(engg_colleges.columns)
+        engg_colleges = pd.concat([engg_colleges,
+                                   pd.get_dummies(engg_colleges[col].apply(pd.Series).stack()).sum(level=0)
+                                   ], axis=1)
+        engg_colleges.columns = [f'{pre}{i}' if i not in college_before else f'{i}' for i in engg_colleges.columns]
+
+    engg_colleges = engg_colleges.drop(['College Name','Campus Size', 'Total Faculty',
+                                        'Established Year', 'Courses', 'Facilities',
+                                        'Country','Genders Accepted', 'University',
+                                        'City', 'State', 'College Type', 'Courses_list',
+                                        'Facilities_list'],
+                                        axis = 1)
+    engg_colleges = engg_colleges.fillna(0)
+
+    neigh = NearestNeighbors(n_neighbors=5, algorithm='brute')
+    neigh.fit(engg_colleges)
+
+    model_params_dict = {}
+    for col in engg_colleges.columns:
+        model_params_dict[col] = 0
 
     return {
         "train_data": train_data,
-        "knn_model": knn_model,
+        "knn_model": neigh,
         "model_params_dict": model_params_dict
     }
 
 def read_json():
     json_file = open("./templates/json/filter_columns_data.json")
     data = json.load(json_file)
+    college_name_list = json.load(open('./templates/json/all_colleges_names.json'))["college_names"]
     
-    return data
+    return data, college_name_list
 
 def predict_model(data_dict, data: dict):
-    data = data['data']
     data = pd.DataFrame(data)
-    distances, indices = data_dict['knn_model'].kneighbors(data) 
-    return data_dict['train_data'][indices[0]]
+    distances, indices = data_dict['knn_model'].kneighbors(data)
+    return data_dict['train_data'].iloc[indices[0]]
+
+def ohe_data(data: pd.DataFrame):
+    data_df = data.copy()
+    print(data_df)
+    ohe_prefix_dict = {
+        'Facilities_list': 'facility_',
+        'Courses_list': 'course_',
+        'University': 'uni_',
+        'City': 'city_',
+        'State': 'state_',
+        'College Type': 'ctype_',
+        'Genders Accepted': 'gender_',
+    }
+    for col, pre in ohe_prefix_dict.items():
+        # Apply one-hot encoding on the list in the row
+        college_before = list(data_df.columns)
+        data_df = pd.concat([data_df,
+                          pd.get_dummies(data_df[col].apply(pd.Series).stack()).sum(level=0)
+                          ], axis=1)
+        data_df.columns = [f'{pre}{i}' if i not in college_before else f'{i}' for i in data_df.columns]
+        data_df.drop(columns=[col], inplace=True)
+    print('in ohe_data', data_df)
+    return data_df
 
